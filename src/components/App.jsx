ab3ea5d742f05f4553b0fdd82408ea8127617222
Alberto Roldán <a-f-r@hotmail.es>
2021-02-27 01:44:35 +0100

    Show current time inside h1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
     return (
         <div className="container">
-      <h1>TIME</h1>
+      <h1>{new Date().toLocaleTimeString('en-US', { hour12: false, 
+                                             hour: "numeric", 
+                                             minute: "numeric"})}</h1>
       <button>Get Time</button>
     </div>
     )
